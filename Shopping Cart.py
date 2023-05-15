12c645257360781ae2783b33714889c1d1de8a28
bitmap357 <edakoyeram@gmail.com>
2023-05-15 12:13:16 +0000

    Inputing items into the cart using a for loop

---
+++ Shopping Cart.py
@@ -0,0 +1,8 @@
+cart = []
+n = int(input('Enter the number of items you would like to input into the cart'))
+
+for x in range(n):
+    item = input('Enter an item into the cart: ')
+    cart.append(item)
+    print(cart)
+

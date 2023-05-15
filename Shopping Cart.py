708199ba4bf875cdd7a57de889cd0a46914c116d
bitmap357 <edakoyeram@gmail.com>
2023-05-15 12:21:00 +0000

    Inputting items into the cart using a while loop

--- Shopping Cart.py
+++ Shopping Cart.py
@@ -1,8 +1,16 @@
 cart = []
-n = int(input('Enter the number of items you would like to input into the cart'))
+# n = int(input('Enter the number of items you would like to input into the cart'))
+#
+# for x in range(n):
+#     item = input('Enter an item into the cart: ')
+#     cart.append(item)
+#     print(cart)
 
-for x in range(n):
-    item = input('Enter an item into the cart: ')
-    cart.append(item)
-    print(cart)
-
+while True:
+    choice = input('Do you want to enter an item to the cart? (yes / no) \n>>>')
+    if choice.lower() == "yes":
+        item = input('Enter an item into the cart: ')
+        cart.append(item)
+        print(f"Current cart contents are {cart}")
+    else:
+        break

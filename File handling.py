30edaacbc6916cfdd230870609f733fc873acb6d
bitmap357 <edakoyeram@gmail.com>
2023-05-16 21:29:08 +0000

    Writing data to a file

--- File handling.py
+++ File handling.py
@@ -1,5 +1,13 @@
 # Opening a file
-file = open('data.txt', 'r')
-content = file.readline()
-print(content)
+
+# Reading data
+# file = open('data.txt', 'r')
+# content = file.readline()
+# print(content)
+# file.close()
+
+# Writing to a file
+file = open('data.txt', 'a')
+content = '\nThis is a third line'
+file.write(content)
 file.close()

6dcf3dd2e4df6b1e92d9925ee1e1dcbc3a707777
bitmap357 <edakoyeram@gmail.com>
2023-05-16 16:16:49 +0000

    Exception handling

--- Errors & Exceptions.py
+++ Errors & Exceptions.py
@@ -1,7 +1,19 @@
 # Syntax errors: error with the construction of the codes
 # Logical error: error with the functionality of the code
-# Runtime error
+# Runtime error: error that occurs when the program is executing
 # Try & except block
+
+n = int(input('Enter numerator'))
+d = int(input('Enter denominator'))
+result = 0
+
+try:
+    result = n / d
+
+except ZeroDivisionError:
+    print("Cannot divide a number by zero")
+
+print(result)
 # Else block
 # Finally block
 

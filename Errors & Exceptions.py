290f42ec3dc6a9820e30dc2848950dfefdff6c88
bitmap357 <edakoyeram@gmail.com>
2023-05-16 16:01:31 +0000

    Commenting on the different types of errors

---
+++ Errors & Exceptions.py
@@ -0,0 +1,7 @@
+# Syntax errors: error with the construction of the codes
+# Logical error: error with the functionality of the code
+# Runtime error
+# Try & except block
+# Else block
+# Finally block
+

43f6e04f8ee22e2d5e8b5e59d4a47f2dbe6bd9c6
bitmap357 <edakoyeram@gmail.com>
2023-05-16 15:33:56 +0000

    Datetime module

--- Modules.py
+++ Modules.py
@@ -8,7 +8,19 @@
 
 # Using inbuilt modules
 
-from random import randint
+# from random import randint
+#
+# result = randint(1, 10)
+# print(result)
 
-result = randint(1, 10)
-print(result)
+# Datetime module
+
+from datetime import datetime, date
+
+today_date = date.today()
+current_time = datetime.now()
+print(today_date)
+print(current_time.time())
+
+
+

9cd58945215ef12f8774935f1cfe2d3c5f712cc6
bitmap357 <edakoyeram@gmail.com>
2023-05-17 21:29:21 +0000

    Functional & OOP Based Way of Writing Code; OOP way

--- Object Oriented Programing.py
+++ Object Oriented Programing.py
@@ -54,10 +54,29 @@
 
 # Functional & OOP Based Way of Writing Code
 # Functional way
-def product_data():
-    product_name = input('Enter name of the product')
-    product_price = input('Enter price of the product')
-    print(product_name)
-    print(product_price)
+# def product_data():
+#     product_name = input('Enter name of the product')
+#     product_price = input('Enter price of the product')
+#     print(product_name)
+#     print(product_price)
+#
+# product_data()
 
-product_data()
+# OOP way
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def get_data(self):
+        self.name = input('Enter name of the product')
+        self.price = input('Enter price of the product')
+
+    def put_data(self):
+        print(self.name)
+        print(self.price)
+
+
+p1 = Product("", "")
+p1.get_data()
+p1.put_data()

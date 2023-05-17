13f0c84753740aecb4beafa82f15cad27ab6ed17
bitmap357 <edakoyeram@gmail.com>
2023-05-17 12:39:03 +0000

    Generator functions

--- Lambdas, Map & Filter.py
+++ Lambdas, Map & Filter.py
@@ -32,9 +32,29 @@
 # print(cap_names)
 
 # Filters
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+#
+# odd_numbers = list(filter(lambda x: x % 2 == 1, numbers))
+# print(odd_numbers)
 
 
-odd_numbers = list(filter(lambda x: x % 2 == 1, numbers))
-print(odd_numbers)
+# Generator functions
+
+# def function():
+#     counter = 0
+#     while counter <= 10:
+#         yield counter
+#         counter += 1
+#
+#
+# print(list(function()))
+
+def even_generator(x):
+    for i in range(x):
+        if i % 2 == 0:
+            yield i
+
+
+print(list(even_generator(11)))
 

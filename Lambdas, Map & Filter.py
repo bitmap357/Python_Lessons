78cc0705f2c6c4bf88d97e8c7f4b3b151f54acf1
bitmap357 <edakoyeram@gmail.com>
2023-05-17 12:12:22 +0000

    Different ways of using map

--- Lambdas, Map & Filter.py
+++ Lambdas, Map & Filter.py
@@ -6,12 +6,23 @@
 # print(result)
 
 # Map functions
-numbers = [1, 2, 3, 4, 5]
+# numbers = [1, 2, 3, 4, 5]
+#
+#
+# def square(x):
+#     return x*x
+#
+#
+# new_list = list(map(square, numbers))
+# print(new_list)
 
+# Different ways of using map
+numbers = ['1', '2', '3', '4', '5']
 
-def square(x):
-    return x*x
-
-
-new_list = list(map(square, numbers))
+new_list = list(map(int, numbers))
 print(new_list)
+
+prices = [100, 200, 300, 400, 500]
+
+result = list(map(lambda x: x - x*5/100, prices))
+print(result)

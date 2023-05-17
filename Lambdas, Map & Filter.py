80710619eecd507a769ae08e8ede8ddb35172405
bitmap357 <edakoyeram@gmail.com>
2023-05-17 11:39:35 +0000

    Map functions

--- Lambdas, Map & Filter.py
+++ Lambdas, Map & Filter.py
@@ -2,5 +2,16 @@
 # result = (lambda x: x**2)(7)
 # print(result)
 
-result = (lambda x=10, y=20: x+y)(y=50)
-print(result)
+# result = (lambda x=10, y=20: x+y)(y=50)
+# print(result)
+
+# Map functions
+numbers = [1, 2, 3, 4, 5]
+
+
+def square(x):
+    return x*x
+
+
+new_list = list(map(square, numbers))
+print(new_list)

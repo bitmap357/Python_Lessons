c0e087c6d7bfd70c0ca2750ac2560b1931d4f25e
bitmap357 <edakoyeram@gmail.com>
2023-05-17 21:19:25 +0000

    Adding inputs to Methods in OOP

--- Object Oriented Programing.py
+++ Object Oriented Programing.py
@@ -36,17 +36,17 @@ class Product:
         self.name = name
         self.price = price
 
-    def summer_discount(self):
-        self.price = self.price - self.price * 5 / 100
+    def summer_discount(self, discount_percent):
+        self.price = self.price - self.price * discount_percent / 100
 
 
 p1 = Product("Tshirt", 10)
 print(p1.name)
 print(p1.price)
-p1.summer_discount()
+p1.summer_discount(10)
 print(p1.price)
 
 
 p2 = Product('Phone', 400)
-p2.summer_discount()
+p2.summer_discount(10)
 print(p2.price)

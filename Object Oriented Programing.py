8fe205b0f8234fc586408feb4747479cf1af3579
bitmap357 <edakoyeram@gmail.com>
2023-05-17 21:16:57 +0000

    Methods in OOP

--- Object Oriented Programing.py
+++ Object Oriented Programing.py
@@ -11,6 +11,24 @@
 
 
 # Creating instance attributes
+# class Product:
+#     quantity = 200
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# p1 = Product("Phone", "300")
+# print(p1.name)
+# print(p1.price)
+#
+# p2 = Product("Laptop", "900")
+# print(p2.name)
+# print(p2.price)
+
+
+# Methods in OOP
 class Product:
     quantity = 200
 
@@ -18,14 +36,17 @@ class Product:
         self.name = name
         self.price = price
 
+    def summer_discount(self):
+        self.price = self.price - self.price * 5 / 100
 
-p1 = Product("Phone", "300")
+
+p1 = Product("Tshirt", 10)
 print(p1.name)
 print(p1.price)
+p1.summer_discount()
+print(p1.price)
 
-p2 = Product("Laptop", "900")
-print(p2.name)
+
+p2 = Product('Phone', 400)
+p2.summer_discount()
 print(p2.price)
-
-
-

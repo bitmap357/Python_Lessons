a2c1b948f521f27e31e8e8967ad0bcef7f30c433
bitmap357 <edakoyeram@gmail.com>
2023-05-18 23:54:21 +0000

    Metacharacters in regular expressions

--- Regular Expressions.py
+++ Regular Expressions.py
@@ -13,10 +13,15 @@
 #     print('No match found')
 
 # Search
-import re
-string = "bca"
-pattern = "a"
-if re.search(pattern, string):
-    print('Match found')
-else:
-    print('No match found')
+# import re
+# string = "bca"
+# pattern = "a"
+# if re.search(pattern, string):
+#     print('Match found')
+# else:
+#     print('No match found')
+
+# Metacharacters in regular expressions
+# They are a special set of characters which don't match themselves, instead, they create some pattern which should be matched.
+# Examples: *, +, {...}, ., ?, ^
+# * - makes sure that a particular character preceding it is present 0 or more times

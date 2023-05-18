ac43f3ecd1cbcd4467ff8d37209706f62b656824
bitmap357 <edakoyeram@gmail.com>
2023-05-18 23:39:56 +0000

    First regular expressions

--- Regular Expressions.py
+++ Regular Expressions.py
@@ -1,3 +1,12 @@
 # Regular Expressions are used to search for patterns in a string or text
 # Raw strings are what regular expressions are and what they are used on
-print(r"Hello \n World")
+# print(r"Hello \n World")
+
+# import the regular expression module
+import re
+string = "abc"
+pattern = "a"
+if re.match(pattern, string):
+    print('Match found')
+else:
+    print('No match found')

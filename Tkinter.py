9bbda6da34839a186316d9fc4020bb55d95d0440
bitmap357 <edakoyeram@gmail.com>
2023-05-20 22:37:56 +0000

    Beverage selector project start

--- Tkinter.py
+++ Tkinter.py
@@ -11,12 +11,12 @@ from tkinter import *
 #     result = str(n1+n2)
 #     answer.config(text='Answer is: ' + result)
 
-def selected():
-    label.config(text=check_value.get())
+# def selected():
+#     label.config(text=check_value.get())
 
 
-root = Tk()
-root.geometry('300x300')
+# root = Tk()
+# root.geometry('300x300')
 # Label
 # hello = Label(root, text='Hello World', fg='red', bg='white', font=('Ariel', 16))
 # hello.pack()
@@ -42,12 +42,18 @@ root.geometry('300x300')
 # answer.pack()
 
 # Checkboxes
-check_value = BooleanVar()
-checkbutton = Checkbutton(root, text='Accept terms', variable=check_value, command=selected)
-checkbutton.pack()
+# check_value = BooleanVar()
+# checkbutton = Checkbutton(root, text='Accept terms', variable=check_value, command=selected)
+# checkbutton.pack()
+#
+# label = Label(root)
+# label.pack()
+#
+#
+# root.mainloop()
 
-label = Label(root)
-label.pack()
-
+# Beverage Selector
+root = Tk()
+root.geometry('300x300')
 
 root.mainloop()

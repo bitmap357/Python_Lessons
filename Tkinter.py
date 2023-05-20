e078707ed55e4df3f3c1598d3b0681c07e8e607b
bitmap357 <edakoyeram@gmail.com>
2023-05-20 21:57:56 +0000

    Button widget

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,16 @@
 from tkinter import *
+
+def display():
+    print('Button clicked')
+
+
 root = Tk()
 root.geometry('300x300')
-hello = Label(root, text='Hello World', fg='red', bg='white', font=('Ariel', 16))
-hello.pack()
+# Label
+# hello = Label(root, text='Hello World', fg='red', bg='white', font=('Ariel', 16))
+# hello.pack()
+
+# Button
+button = Button(root, text='Click here', command=display)
+button.pack()
 root.mainloop()

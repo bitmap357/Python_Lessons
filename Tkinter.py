ecf2da924a5adaaa71b23be2eeb3fc51c2260939
bitmap357 <edakoyeram@gmail.com>
2023-05-20 22:23:30 +0000

    Adding two numbers

--- Tkinter.py
+++ Tkinter.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
 
-def display():
-    data = entry.get()
-    print(data)
+# def display():
+#     data = entry.get()
+#     print(data)
+
+def add():
+    n1 = int(number1.get())
+    n2 = int(number2.get())
+    print(n1+n2)
 
 
 root = Tk()
@@ -13,12 +18,20 @@ root.geometry('300x300')
 # hello.pack()
 
 # User input
-entry = Entry(root)
-entry.pack()
+# entry = Entry(root)
+# entry.pack()
 
 # Button
-button = Button(root, text='Click here', command=display)
-button.pack()
+# button = Button(root, text='Click here', command=display)
+# button.pack()
 
+# Adding 2 numbers
+number1 = Entry(root)
+number2 = Entry(root)
+number1.pack()
+number2.pack()
+
+button = Button(root, text='Add', command=add)
+button.pack()
 
 root.mainloop()

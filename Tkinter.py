783dd472c9076330b4f1aec2b73462742f5eceb5
bitmap357 <edakoyeram@gmail.com>
2023-05-20 21:53:49 +0000

    Customizing the label widget

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root = Tk()
-root.geometry('300x400')
-hello = Label(root, text='Hello World')
+root.geometry('300x300')
+hello = Label(root, text='Hello World', fg='red', bg='white', font=('Ariel', 16))
 hello.pack()
 root.mainloop()

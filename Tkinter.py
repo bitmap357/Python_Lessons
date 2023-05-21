2acc4b6dce6496f1094a43dc559b092c0a42bc59
bitmap357 <edakoyeram@gmail.com>
2023-05-21 19:54:35 +0000

    Creating a grid

--- Tkinter.py
+++ Tkinter.py
@@ -134,23 +134,33 @@ from tkinter import *
 # root.mainloop()
 
 # Grid Layout
+# root = Tk()
+# label1 = Label(root, text='Email')
+# label2 = Label(root, text='Password')
+#
+# text1 = Entry(root)
+# text2 = Entry(root)
+#
+# label1.grid(row=0, column=0)
+# text1.grid(row=0, column=1)
+#
+# label2.grid(row=1, column=0)
+# text2.grid(row=1, column=1)
+#
+# button = Button(root, text='Login')
+# button.grid(column=1, row=2)
+#
+# root.geometry('300x300')
+#
+# root.mainloop()
 
+# Crating a Grid
 root = Tk()
-label1 = Label(root, text='Email')
-label2 = Label(root, text='Password')
-
-text1 = Entry(root)
-text2 = Entry(root)
-
-label1.grid(row=0, column=0)
-text1.grid(row=0, column=1)
-
-label2.grid(row=1, column=0)
-text2.grid(row=1, column=1)
-
-button = Button(root, text='Login')
-button.grid(column=1, row=2)
-
-root.geometry('300x300')
+for x in range(3):
+    for y in range(3):
+        frame = Frame(root)
+        frame.grid(row=x, column=y)
+        button = Button(frame, text=f'Row {x} \n Column {y}')
+        button.pack(pady=5, padx=5)
 
 root.mainloop()

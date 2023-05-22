ad24f4dde549a43fbbfbad4fb0751ff2687ac81a
bitmap357 <edakoyeram@gmail.com>
2023-05-22 21:51:20 +0000

    Buttons for the Calculator App

--- Calculator App.py
+++ Calculator App.py
@@ -4,5 +4,12 @@ root = Tk()
 display = Entry(root)
 display.grid(row=1, columnspan=6)
 
+counter = 1
+for x in range(3):
+    for y in range(3):
+        button = Button(root, text=counter, width=2, height=2)
+        button.grid(row=x+2, column=y)
+        counter += 1
+
 
 root.mainloop()

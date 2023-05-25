16348e9007ce6d74161c1752687941e91b5d7dd3
bitmap357 <edakoyeram@gmail.com>
2023-05-25 21:53:11 +0000

    Upload file button

--- File Upload App.py
+++ File Upload App.py
@@ -5,4 +5,6 @@ root = Tk()
 main_label = Label(root, text='WELCOME\n WHAT WOULD YOU LIKE TO DO TODAY?', font=('Times New Roman', '32'))
 main_label.grid(row=0, column=0)
 
+upload_file_button = Button(root, text='UPLOAD FILE')
+
 root.mainloop()

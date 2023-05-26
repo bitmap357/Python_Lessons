c8d51e9f6c25e413d3f53f6117a9e92cf8f7ab9c
bitmap357 <edakoyeram@gmail.com>
2023-05-26 23:27:44 +0000

    Comment for button for file uploads

--- File Upload App.py
+++ File Upload App.py
@@ -7,7 +7,7 @@ root.geometry('880x500')
 main_label = Label(root, text='WELCOME\n WHAT WOULD YOU LIKE TO DO TODAY?', font=('Times New Roman', '32'))
 main_label.grid(row=0, column=0, pady=20, padx=10)
 
-
+# Button for file uploads
 upload_file_button = Button(root, text='UPLOAD FILE', pady=20, padx=40)
 upload_file_button.grid(row=2, column=0, padx=10, pady=10)
 

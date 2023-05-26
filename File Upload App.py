1ed8bddee892b8a0df826c333c8e510a10e6ebe0
bitmap357 <edakoyeram@gmail.com>
2023-05-26 23:28:18 +0000

    Comment for button for browsing files

--- File Upload App.py
+++ File Upload App.py
@@ -11,6 +11,7 @@ main_label.grid(row=0, column=0, pady=20, padx=10)
 upload_file_button = Button(root, text='UPLOAD FILE', pady=20, padx=40)
 upload_file_button.grid(row=2, column=0, padx=10, pady=10)
 
+# Button for browsing files
 browse_files_button = Button(root, text='BROWSE FILES', pady=20, padx=40)
 browse_files_button.grid(row=3, column=0, padx=10, pady=10)
 

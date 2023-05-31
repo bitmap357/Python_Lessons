3dceb434601000618c124b4680d075353db45a9a
bitmap357 <edakoyeram@gmail.com>
2023-05-31 14:31:33 +0000

    Labels for text inputs

--- Database Test for App.py
+++ Database Test for App.py
@@ -41,6 +41,20 @@ state.grid(row=4, column=1)
 zipcode = Entry(root, width=30)
 zipcode.grid(row=5, column=1)
 
+# Create text box labels
+f_name_label = Label(root, text='First Name')
+f_name_label.grid(row=0, column=0)
+l_name_label = Label(root, text='Last Name')
+l_name_label.grid(row=1, column=0)
+address_label = Label(root, text='Address')
+address_label.grid(row=2, column=0)
+city_label = Label(root, text='City')
+city_label.grid(row=3, column=0)
+state_label = Label(root, text='State')
+state_label.grid(row=4, column=0)
+zipcode_label = Label(root, text='Zip code')
+zipcode_label.grid(row=5, column=0)
+
 # Commit changes
 conn.commit()
 

18d93dafd032c58bf8b1c46dbd8183fb9c75c4dd
bitmap357 <edakoyeram@gmail.com>
2023-06-19 22:00:24 +0000

    Creating a table

--- Demo.py
+++ Demo.py
@@ -3,6 +3,6 @@ import psycopg2
 conn = psycopg2.connect(dbname="postgres", user="postgres", password="snow9823", host="localhost", port="5432")
 
 cur = conn.cursor()
-
+cur.execute('''CREATE TABLE student(NAME TEXT, AGE TEXT, ADDRESS TEXT)''')
 
 print('connection success')

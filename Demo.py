d2902fcebc65170777859189358721478bf163d3
bitmap357 <edakoyeram@gmail.com>
2023-06-19 21:58:54 +0000

    Creating a cursor

--- Demo.py
+++ Demo.py
@@ -2,4 +2,7 @@ import psycopg2
 
 conn = psycopg2.connect(dbname="postgres", user="postgres", password="snow9823", host="localhost", port="5432")
 
+cur = conn.cursor()
+
+
 print('connection success')

9b735dbc7142ec256f4ff4f990307e53ea798678
bitmap357 <edakoyeram@gmail.com>
2023-06-22 19:13:53 +0000

    creating a function for the table creation

--- Demo.py
+++ Demo.py
@@ -1,10 +1,11 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname="postgres!", user="postgres", password="snow9823", host="localhost", port="5432")
 
-cur = conn.cursor()
-cur.execute('''CREATE TABLE student(ID SERIAL, NAME TEXT, AGE TEXT, ADDRESS TEXT);''')
-print('table created')
-conn.commit()
-conn.close()
+def create():
+    conn = psycopg2.connect(dbname="postgres!", user="postgres", password="snow9823", host="localhost", port="5432")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE student(ID SERIAL, NAME TEXT, AGE TEXT, ADDRESS TEXT);''')
+    print('table created')
+    conn.commit()
+    conn.close()
 

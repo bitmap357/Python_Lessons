b9948fb0876d1b898470e11aba64a9c0fd64eb08
bitmap357 <edakoyeram@gmail.com>
2023-06-22 19:10:44 +0000

    committing the table

--- Demo.py
+++ Demo.py
@@ -5,5 +5,5 @@ conn = psycopg2.connect(dbname="postgres!", user="postgres", password="snow9823"
 cur = conn.cursor()
 cur.execute('''CREATE TABLE student(ID SERIAL, NAME TEXT, AGE TEXT, ADDRESS TEXT);''')
 print('table created')
-
+conn.commit()
 

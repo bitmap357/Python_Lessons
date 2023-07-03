eeb5b12de5c2b7b4227f3967714e1bc04bd8302d
bitmap357 <edakoyeram@gmail.com>
2023-07-03 21:48:32 +0000

    compression function

--- pypro/compressedmodule.py
+++ pypro/compressedmodule.py
@@ -1,1 +1,10 @@
-import zlib, base64
+import zlib,base64
+
+
+def compress(inputfile, outputfile):
+    data = open('demo.txt', 'r').read()
+    data_bytes = bytes(data, 'utf-8')
+    compressed_data = base64.b64encode(zlib.compress(data_bytes, 9))
+    decoded_data = compressed_data.decode('utf-8')
+    compressed_file = open('compressed.txt', 'w')
+    compressed_file.write(decoded_data)

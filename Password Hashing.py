e62585c4cefbc1fbab2d5667b7b1ee8b2472aea0
bitmap357 <edakoyeram@gmail.com>
2023-07-05 18:55:33 +0000

    comparing the passwords

--- Password Hashing.py
+++ Password Hashing.py
@@ -6,3 +6,7 @@ hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 
 print(hashed)
 
+entered_password = input("Enter password to login: ")
+entered_password = bytes(entered_password, encoding='utf-8')
+
+bcrypt.checkpw(entered_password)

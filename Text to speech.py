40674726e80e02036070f8c15cd1e766a2c58430
bitmap357 <edakoyeram@gmail.com>
2023-07-05 16:00:39 +0000

    opening text file

--- Text to speech.py
+++ Text to speech.py
@@ -1,8 +1,11 @@
 from gtts import gTTS
 import os
 
-text = "Today is a good day"
-output = gTTS(text=text, lang='en', slow=False)
-output.save('output.mp3')
+# text = "Today is a good day"
+# output = gTTS(text=text, lang='en', slow=False)
+# output.save('output.mp3')
+#
+# os.system("start output.mp3")
 
-os.system("start output.mp3")
+
+text = open('demo.txt', 'r').read()

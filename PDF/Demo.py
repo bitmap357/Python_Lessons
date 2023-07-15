a5c56ef4393f2e64aeae22af178e1a5c7e76ca1a
bitmap357 <edakoyeram@gmail.com>
2023-07-15 22:17:20 +0000

    adding font to class

--- PDF/Demo.py
+++ PDF/Demo.py
@@ -4,11 +4,11 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         self.image("logo.png", 10, 8, 33)
+        self.set_font("helvetica", "B", 16)
         self.cell(40, 10, "Hello world")
 
 
 pdf = PDF()
 
 pdf.add_page()
-pdf.set_font("helvetica", "B", 16)
 pdf.output("sample.pdf")

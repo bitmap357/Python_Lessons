0d16f0eee273585840b26b3854c82cf619e0c45d
bitmap357 <edakoyeram@gmail.com>
2023-07-15 22:18:15 +0000

    adding cell to class

--- PDF/Demo.py
+++ PDF/Demo.py
@@ -5,6 +5,7 @@ class PDF(FPDF):
     def header(self):
         self.image("logo.png", 10, 8, 33)
         self.set_font("helvetica", "B", 16)
+        self.cell(80)
         self.cell(40, 10, "Hello world")
 
 

1d043ad88071d214d514486f6cbb47e528923f42
bitmap357 <edakoyeram@gmail.com>
2023-07-15 22:19:10 +0000

    edits

--- PDF/Demo.py
+++ PDF/Demo.py
@@ -6,7 +6,7 @@ class PDF(FPDF):
         self.image("logo.png", 10, 8, 33)
         self.set_font("helvetica", "B", 16)
         self.cell(80)
-        self.cell(40, 10, "Hello world")
+        self.cell(40, 10, "Hello world", border=1, align="C")
 
 
 pdf = PDF()

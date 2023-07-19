5292242e05016b82a94a00de70f48c180b9fae3f
bitmap357 <edakoyeram@gmail.com>
2023-07-19 22:03:32 +0000

    error correction

--- PDF/countries_pdf.py
+++ PDF/countries_pdf.py
@@ -15,7 +15,7 @@ with pdf.table(
     borders_layout="NO_HORIZONTAL_LINES",
     cell_fill_color=(224, 235, 255),
     col_widths=(42, 39, 35, 42),
-    line_hight=6,
+    line_height=6,
     headings_style=headings_style,
     text_align=("LEFT", "CENTER", "RIGHT", "RIGHT"),
     width=160

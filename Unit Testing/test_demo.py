7490818a731978b31f750f8b3c3e3ecec46401c8
bitmap357 <edakoyeram@gmail.com>
2023-07-27 14:13:32 +0000

    assert statement

--- Unit Testing/test_demo.py
+++ Unit Testing/test_demo.py
@@ -7,6 +7,7 @@ class TestDemo(unittest.TestCase):
     def test_add(self):
         self.assertEquals(Demo.add(2, 2), 4)
         self.assertEquals(Demo.add(10, 4), 14)
+        self.assertEquals(Demo.add(23, 7), 30)
 
 
 

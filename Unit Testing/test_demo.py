dd1879f2b1d7ac23bc9be1464fc506884dc62cb5
bitmap357 <edakoyeram@gmail.com>
2023-07-27 14:12:36 +0000

    assert function

--- Unit Testing/test_demo.py
+++ Unit Testing/test_demo.py
@@ -6,6 +6,8 @@ class TestDemo(unittest.TestCase):
 
     def test_add(self):
         self.assertEquals(Demo.add(2, 3), 5)
+        self.assertEquals(Demo.add(10, 4), 14)
+
 
 
 if __name__ == '__main__':

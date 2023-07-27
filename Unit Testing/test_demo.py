38f0a41d48237ee8639441d48e4fc380bec9be0d
bitmap357 <edakoyeram@gmail.com>
2023-07-27 14:26:41 +0000

    creating a class

--- Unit Testing/test_demo.py
+++ Unit Testing/test_demo.py
@@ -1,28 +1,28 @@
-import unittest
-import Demo
-
-
-class TestDemo(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEquals(Demo.add(2, 2), 4)
-        self.assertEquals(Demo.add(10, 4), 14)
-        self.assertEquals(Demo.add(23, 7), 30)
-
-    def test_sub(self):
-        self.assertEquals(Demo.sub(10, 5), 5)
-        self.assertEquals(Demo.sub(4, 5), -1)
-
-    def test_mul(self):
-        self.assertEquals(Demo.mul(2, 3), 6)
-        self.assertEquals(Demo.mul(10, 4), 40)
-        self.assertEquals(Demo.mul(2, 7), 14)
-
-    def test_div(self):
-        self.assertEquals(Demo.div(4, 2), 2)
-        self.assertEquals(Demo.div(12, 4), 3)
-        self.assertEquals(Demo.div(25, 5), 5)
-
-
-if __name__ == '__main__':
-    unittest.main()
+# import unittest
+# import Demo
+#
+#
+# class TestDemo(unittest.TestCase):
+#
+#     def test_add(self):
+#         self.assertEquals(Demo.add(2, 2), 4)
+#         self.assertEquals(Demo.add(10, 4), 14)
+#         self.assertEquals(Demo.add(23, 7), 30)
+#
+#     def test_sub(self):
+#         self.assertEquals(Demo.sub(10, 5), 5)
+#         self.assertEquals(Demo.sub(4, 5), -1)
+#
+#     def test_mul(self):
+#         self.assertEquals(Demo.mul(2, 3), 6)
+#         self.assertEquals(Demo.mul(10, 4), 40)
+#         self.assertEquals(Demo.mul(2, 7), 14)
+#
+#     def test_div(self):
+#         self.assertEquals(Demo.div(4, 2), 2)
+#         self.assertEquals(Demo.div(12, 4), 3)
+#         self.assertEquals(Demo.div(25, 5), 5)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
